6116786479b982fd1b6e2a1469732691a7493eba
Максим Наконечный <gembrilus@gmail.com>
2020-07-25 00:45:35 +0300

    UPD

--- 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
@@ -24,9 +24,32 @@ public class Solution {
         map.put("X", 10);
         map.put("L", 50);
         map.put("C", 100);
-        map.put("D", 400);
+        map.put("D", 500);
         map.put("M", 1000);
 
-        return 0;
+        String[] chars = new StringBuilder(s)
+                .reverse()
+                .toString()
+                .split("");
+        String previous = null;
+        int result = 0;
+
+        for (String ch: chars){
+            int current = map.get(ch);
+            if (previous == null){
+                previous = ch;
+                result = current;
+            } else {
+                int prev = map.get(previous);
+                if (prev <= current){
+                    result += current;
+                } else {
+                    result -= current;
+                }
+                previous = ch;
+            }
+        }
+
+        return result;
     }
 }
